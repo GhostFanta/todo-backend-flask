cdf087088aef502d18a97a1cc1f937a55f7b7cba
GhostFanta <huiyueguan111@gmail.com>
2018-07-25 18:09:19 -0300

    define model

---
+++ dao/operation.py
@@ -0,0 +1,33 @@
+from flask import Blueprint
+from flask_restful import Resource, Api
+
+from flask import jsonify
+
+core_blueprint = Blueprint('core', __name__)
+apiserver = Api(core_blueprint)
+
+
+class TodoList(Resource):
+    def get(self):
+        """
+        Get all todolists or get by id
+        :return:
+        """
+        pass
+
+    def post(self):
+        """
+        Create new todolist
+        :return:
+        """
+        pass
+
+    def delete(self):
+        """
+        Delete todolist accouding todolist id
+        :return:
+        """
+        pass
+
+
+apiserver.add_resource(TodoList, '/todolist')
