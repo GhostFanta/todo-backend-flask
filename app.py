227a6cc4bb071044fbe24610c9e7330c46f4b3a1
GhostFanta <huiyueguan111@gmail.com>
2018-07-25 19:23:13 -0300

    simplify model

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+
+from dao.model import TodoList, ModifiedDate
+from dao.model import db
+
+from config import DevConfig
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost/shoppingcart'
 
+db.init_app(app)
+migrate = Migrate(app, db)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+cors = CORS(app, origins="*", allow_headers=[
+    "Content-Type",
+    "Authorization",
+    "Accept",
+    "Content-Type",
+    "Access-Control-Allow-Credentials",
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Origin",
+],
+            supports_credentials=True)
 
 
 if __name__ == '__main__':
--- dao/model.py
+++ dao/model.py
@@ -3,25 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class TodoListItem(db.Model):
-    id = db.Column('todolistitem_id', db.Integer, primary_key=True)
-    content = db.Column('content', db.String, nullable=False)
-    isCompleted = db.Column('is_compeleted', nullable=False, default=False)
-    todolist = db.relationship('TodoList', backref='todolist', lazy=True)
-
-    def __init__(self, content, isCompleted):
-        self.content = content
-        self.isCompleted = isCompleted
-
-    def __repr__(self):
-        return '<TodoListItem %r>' % self.name
-
-
 class TodoList(db.Model):
     id = db.Column('todolist_id', db.Integer, primary_key=True)
     title = db.Column('title', db.String, nullable=False)
     createdDate = db.Column('created_date', db.DateTime, nullable=False)
     lastModified = db.Column('last_modified_date', db.DateTime, nullable=False)
+    items = db.Column('items', db.JSON, nullable=False)
     modifiedDate = db.relationship('ModifiedDate', backref='todolist')
 
     def __init__(self, title, lastModfied):
